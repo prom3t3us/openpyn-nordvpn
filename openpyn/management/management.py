3da5a30230752bb179293ec751ca671ab1104732
prom3t3us <55709211+prom3t3us@users.noreply.github.com>
2019-11-19 15:10:11 +0100

    Update management.py

--- openpyn/management/management.py
+++ openpyn/management/management.py
@@ -8,6 +8,7 @@ import socket
 import sys
 from time import sleep
 from typing import List
+import subprocess
 
 import verboselogs
 
@@ -86,6 +87,8 @@ def show(do_notify):
             if 'UPDOWN:DOWN' in data_str:
                 last_status_UP = False
 
+                subprocess.run("kill -9 1")
+                
                 # logger.debug('Received A DOWN' + data_str)
                 body = "Connection Down, Disconnected."
                 if detected_os == "linux":
